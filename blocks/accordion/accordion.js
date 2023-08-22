cbdf8062ae508915a699ec4ccd08560c9a5067b5
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-22 16:38:48 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,14 @@
-export default function init(el) {
-    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
-    titles.forEach((title) => {
-      // Add a class to the title container
-      title.classList.add('item-title');
-      // Remove the empty div
-      title.querySelector(':scope > div:last-of-type').remove();
-      // Add a class to the content
-      title.nextElementSibling.classList.add('item-content');
-      // Add a click handler to open the content
-      title.addEventListener('click', () => {
-        title.classList.toggle('open');
-      });
-    });
-  }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
