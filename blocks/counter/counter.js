00ea45acb493754cbb225cf13b82f6e871a7adc1
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-22 20:27:26 +0530

    Update counter.js

--- blocks/counter/counter.js
+++ blocks/counter/counter.js
@@ -1,29 +1,40 @@
-const counterElements = document.querySelectorAll(".counter");
+// Assuming you want to update the content with the provided HTML
+var updatedHTML = `
+<section id="stats" class="stats">
+  <img src="" alt="" data-aos="fade-in" class="aos-init aos-animate">
+  <div class="container position-relative aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+    <div class="row gy-4">
+      <div class="col-lg-3 col-md-6">
+        <div class="stats-item text-center w-100 h-100">
+          <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="0" class="purecounter">232</span>
+          <p>Clients</p>
+        </div>
+      </div><!-- End Stats Item -->
+      <div class="col-lg-3 col-md-6">
+        <div class="stats-item text-center w-100 h-100">
+          <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="0" class="purecounter">521</span>
+          <p>Projects</p>
+        </div>
+      </div><!-- End Stats Item -->
+      <div class="col-lg-3 col-md-6">
+        <div class="stats-item text-center w-100 h-100">
+          <span data-purecounter-start="0" data-purecounter-end="1453" data-purecounter-duration="0" class="purecounter">1453</span>
+          <p>Hours Of Support</p>
+        </div>
+      </div><!-- End Stats Item -->
+      <div class="col-lg-3 col-md-6">
+        <div class="stats-item text-center w-100 h-100">
+          <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="0" class="purecounter">32</span>
+          <p>Workers</p>
+        </div>
+      </div><!-- End Stats Item -->
+    </div>
+  </div>
+</section>
+`;
 
-       
+// Select the element with the class name 'counter-wrapper'
+var counterWrapper = document.querySelector('.counter-wrapper');
 
-        let count = 0;
-
-        const targetCount = 200;
-
-       
-
-        const interval = setInterval(() => {
-
-            if (count <= targetCount) {
-
-                counterElements.forEach(element => {
-
-                    element.textContent = count;
-
-                });
-
-                count++;
-
-            } else {
-
-                clearInterval(interval);
-
-            }
-
-        }, 50); // Adjust the interval as needed
+// Update the innerHTML of the selected element with the updated HTML content
+counterWrapper.innerHTML = updatedHTML;
