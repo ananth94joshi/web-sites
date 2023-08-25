177895f533048150d22fa96d61dca26dda65eb97
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 22:25:56 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -174,3 +174,9 @@ appendFormContainerForm.classList.add('col-lg-6');
 newRow.appendChild(appendFormContainer);
 newRow.appendChild(appendFormContainerForm);
 appendParentFormContainer.appendChild(newRow);
+//scripts for adding class
+// Select the element using querySelector
+const targetDiv = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div:nth-child(1)");
+
+// Add a class to the selected div
+targetDiv.classList.add("counter");
