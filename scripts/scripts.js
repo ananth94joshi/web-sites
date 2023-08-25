f96b84fbf6c628a5edc1bd48a1a655d1f9aa1a0a
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 23:48:37 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -224,3 +224,19 @@ animateCounter("body > main > div:nth-child(5) > div.columns-wrapper > div > div
 
 // Create counter animation for worker
 animateCounter("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div:nth-child(4)", 32); // Updated worker count
+
+
+//for input on hero banner
+// Find the existing strong tag
+var strongTag = document.querySelector(".button-container strong");
+
+if (strongTag) {
+    // Create a new input element
+    var inputElement = document.createElement("input");
+    inputElement.type = "email"; // Set input type to 'email'
+    inputElement.placeholder = "Enter your email address"; // Set the placeholder text
+    inputElement.className = "enteryouremail"; // Add a class to the input element
+
+    // Add the input element to the existing strong tag
+    strongTag.appendChild(inputElement);
+}
