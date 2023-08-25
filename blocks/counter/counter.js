fca8b3d40cb6a8b95919dec2411aac0a543844d1
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 22:29:00 +0530

    Update counter.js

--- blocks/counter/counter.js
+++ blocks/counter/counter.js
@@ -4,31 +4,3 @@ const targetDiv = document.querySelector("body > main > div:nth-child(5) > div.c
 
 // Add a class to the selected div
 targetDiv.classList.add("counter");
-
-const counterElements = document.querySelectorAll(".counter");
-        let count = 0;
-
-        const targetCount = 200;
-
-       
-
-        const interval = setInterval(() => {
-
-            if (count <= targetCount) {
-
-                counterElements.forEach(element => {
-
-                    element.textContent = count;
-
-                });
-
-                count++;
-
-            } else {
-
-                clearInterval(interval);
-
-            }
-
-        }, 50); // Adjust the interval as needed
-
