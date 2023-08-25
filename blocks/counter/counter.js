9653d4e8aaab38b45447bfa74349a5fdbea65763
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 22:28:01 +0530

    Update counter.js

--- blocks/counter/counter.js
+++ blocks/counter/counter.js
@@ -1,3 +1,10 @@
+//scripts for adding class
+// Select the element using querySelector
+const targetDiv = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div:nth-child(1)");
+
+// Add a class to the selected div
+targetDiv.classList.add("counter");
+
 const counterElements = document.querySelectorAll(".counter");
         let count = 0;
 
