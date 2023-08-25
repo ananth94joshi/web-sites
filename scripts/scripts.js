0fd82f249b2fa0f3c2ad0f49e6109c42477dd567
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 22:30:44 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -174,3 +174,36 @@ appendFormContainerForm.classList.add('col-lg-6');
 newRow.appendChild(appendFormContainer);
 newRow.appendChild(appendFormContainerForm);
 appendParentFormContainer.appendChild(newRow);
+//scripts for adding class
+// Select the element using querySelector
+const targetDiv = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div:nth-child(1)");
+
+// Add a class to the selected div
+targetDiv.classList.add("counter");
+
+const counterElements = document.querySelectorAll(".counter");
+        let count = 0;
+
+        const targetCount = 200;
+
+       
+
+        const interval = setInterval(() => {
+
+            if (count <= targetCount) {
+
+                counterElements.forEach(element => {
+
+                    element.textContent = count;
+
+                });
+
+                count++;
+
+            } else {
+
+                clearInterval(interval);
+
+            }
+
+        }, 50); // Adjust the interval as needed
