4ea11a959aa6914e851107d3e353f79073e7adba
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-25 15:14:47 +0530

    Update counter.js

--- blocks/counter/counter.js
+++ blocks/counter/counter.js
@@ -1,7 +1,4 @@
 const counterElements = document.querySelectorAll(".counter");
-
-       
-
         let count = 0;
 
         const targetCount = 200;
@@ -27,3 +24,102 @@ const counterElements = document.querySelectorAll(".counter");
             }
 
         }, 50); // Adjust the interval as needed
+
+
+
+const counterElements = document.querySelectorAll(".workers");
+
+       
+
+        let count = 0;
+
+        const targetCount = 32;
+
+       
+
+        const interval = setInterval(() => {
+
+            if (count <= targetCount) {
+
+                counterElements.forEach(element => {
+
+                    element.textContent = count;
+
+                });
+
+                count++;
+
+            } else {
+
+                clearInterval(interval);
+
+            }
+
+        }, 50); // Adjust the interval as needed
+
+
+
+
+const counterElements = document.querySelectorAll(".projects");
+
+       
+
+        let count = 0;
+
+        const targetCount = 521;
+
+       
+
+        const interval = setInterval(() => {
+
+            if (count <= targetCount) {
+
+                counterElements.forEach(element => {
+
+                    element.textContent = count;
+
+                });
+
+                count++;
+
+            } else {
+
+                clearInterval(interval);
+
+            }
+
+        }, 50); // Adjust the interval as needed
+const counterElements = document.querySelectorAll(".support");
+
+       
+
+        let count = 0;
+
+        const targetCount = 1453;
+
+       
+
+        const interval = setInterval(() => {
+
+            if (count <= targetCount) {
+
+                counterElements.forEach(element => {
+
+                    element.textContent = count;
+
+                });
+
+                count++;
+
+            } else {
+
+                clearInterval(interval);
+
+            }
+
+        }, 50); // Adjust the interval as needed
+
+
+
+
+
