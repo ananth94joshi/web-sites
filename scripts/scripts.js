283450745a4be2b006d5f984dd4c7b80309293a3
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-26 19:49:55 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -149,6 +149,47 @@ appendFormContainerForm.classList.add('col-lg-6');
 newRow.appendChild(appendFormContainer);
 newRow.appendChild(appendFormContainerForm);
 appendParentFormContainer.appendChild(newRow);
+//bootstrap classes 
+
+
+
+const selectBody = document.querySelector('body');
+
+  function toggleScrolled() {
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  }
+
+  document.addEventListener('scroll', toggleScrolled);
+  window.addEventListener('load', toggleScrolled);
+  
+  // var footerContainer = document.querySelector("body > footer > div > div > div > div");
+  // footerContainer.classList.add('container');
+  var footerRow = document.querySelector("body > footer > div > div > div > div > div");
+  footerRow.classList.add('row', 'gy-4');
+  var footerAbout = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(1)");
+  footerAbout.classList.add('col-lg-5', 'col-md-12');
+  var footerUsefulLinks = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(2)");
+  footerUsefulLinks.classList.add('col-lg-2', 'col-6');
+  var footerServices = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(3)");
+  footerServices.classList.add('col-lg-2', 'col-6');
+  var footerContactUs = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(4)");
+  footerContactUs.classList.add('col-lg-3', 'col-md-12');
+  
+  var testimonialContainer = document.querySelector("body > main > div:nth-child(13) > div");
+  var testimonialsTitle = document.querySelector("body > main > div:nth-child(13) > div.default-content-wrapper > h2#testimonials");
+  var testimonialDesc = document.querySelector("body > main > div:nth-child(13) > div.default-content-wrapper > p:nth-child(2)");
+  var testimonialCaro = document.querySelector("body > main > div:nth-child(13) > div > p:nth-child(3)");
+  var testimonialNewRow = document.createElement('div');
+  testimonialNewRow.classList.add('col-lg-5');
+  testimonialNewRow.appendChild(testimonialsTitle);
+  testimonialNewRow.appendChild(testimonialDesc);
+  testimonialContainer.appendChild(testimonialNewRow);
+  var testimonialNewCarousal = document.createElement('div');
+  testimonialNewCarousal.classList.add('col-lg-7');
+  testimonialNewCarousal.appendChild(testimonialCaro);
+  testimonialContainer.appendChild(testimonialNewCarousal);
+  testimonialContainer.classList.add('row');
+  
 
 // Find the existing strong tag in hero for updating input
 var strongTag = document.querySelector(".button-container strong");
