9606c2a284a1f67008767cb076ba55372fdbe5a9
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-26 19:51:59 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -164,16 +164,16 @@ const selectBody = document.querySelector('body');
   
   // var footerContainer = document.querySelector("body > footer > div > div > div > div");
   // footerContainer.classList.add('container');
-  var footerRow = document.querySelector("body > footer > div > div > div > div > div");
-  footerRow.classList.add('row', 'gy-4');
-  var footerAbout = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(1)");
-  footerAbout.classList.add('col-lg-5', 'col-md-12');
-  var footerUsefulLinks = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(2)");
-  footerUsefulLinks.classList.add('col-lg-2', 'col-6');
-  var footerServices = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(3)");
-  footerServices.classList.add('col-lg-2', 'col-6');
-  var footerContactUs = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(4)");
-  footerContactUs.classList.add('col-lg-3', 'col-md-12');
+  // var footerRow = document.querySelector("body > footer > div > div > div > div > div");
+  // footerRow.classList.add('row', 'gy-4');
+  // var footerAbout = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(1)");
+  // footerAbout.classList.add('col-lg-5', 'col-md-12');
+  // var footerUsefulLinks = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(2)");
+  // footerUsefulLinks.classList.add('col-lg-2', 'col-6');
+  // var footerServices = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(3)");
+  // footerServices.classList.add('col-lg-2', 'col-6');
+  // var footerContactUs = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(4)");
+  // footerContactUs.classList.add('col-lg-3', 'col-md-12');
   
   var testimonialContainer = document.querySelector("body > main > div:nth-child(13) > div");
   var testimonialsTitle = document.querySelector("body > main > div:nth-child(13) > div.default-content-wrapper > h2#testimonials");
