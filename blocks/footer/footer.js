5f3adf31b78bb70b28895950a8ef18e93568b6d5
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-26 20:13:34 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -17,3 +17,16 @@ export default async function decorate(block) {
   await decorateIcons(footer);
   block.append(footer);
 }
+
+  var footerContainer = document.querySelector("body > footer > div > div > div > div");
+  footerContainer.classList.add('container');
+  var footerRow = document.querySelector("body > footer > div > div > div > div > div");
+  footerRow.classList.add('row', 'gy-4');
+  var footerAbout = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(1)");
+  footerAbout.classList.add('col-lg-5', 'col-md-12');
+  var footerUsefulLinks = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(2)");
+  footerUsefulLinks.classList.add('col-lg-2', 'col-6');
+  var footerServices = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(3)");
+  footerServices.classList.add('col-lg-2', 'col-6');
+  var footerContactUs = document.querySelector("body > footer > div > div > div > div > div > div:nth-child(4)");
+  footerContactUs.classList.add('col-lg-3', 'col-md-12');
