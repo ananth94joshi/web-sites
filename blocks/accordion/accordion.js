0e519c2b764061a815d036ef990bc2eca9543b49
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-26 18:44:26 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,7 +4,7 @@ export default function init(el) {
       // Add a class to the title container
       title.classList.add('item-title');
       // Remove the empty div
-      title.querySelector(':scope > div:last-of-type').remove();
+     // title.querySelector(':scope > div:last-of-type').remove();
       // Add a class to the content
       title.nextElementSibling.classList.add('item-content');
       // Add a click handler to open the content
@@ -12,13 +12,7 @@ export default function init(el) {
         title.classList.toggle('open');
       });
         // Select the source element
-var sourceElement = document.querySelector("#non-consectetur-a-erat-nam-at-lectus-urna-duis");
 
-// Select the target element
-var targetElement = document.querySelector("body > main > div.section.accordion-container > div:nth-child(2) > div > div.item-title");
-
-// Move the content from the source element to the target element
-targetElement.appendChild(sourceElement);
     });
   }
 
