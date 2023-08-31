d035263eea3d71c64130b4d301a28a60cc265bba
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-08-31 17:11:45 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -290,3 +290,23 @@ function updateContainer() {
 		FeaturesContainer.appendChild(FeaturesimageMobile);
 	}
 }
+
+//to move h3
+// Select the elements
+var clientsElement = document.querySelector("#clients");
+var targetElement = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div.client.counter.worker");
+
+ 
+
+// Check if both elements are valid
+if (clientsElement && targetElement) {
+    // Remove the #clients element from its current position
+    clientsElement.parentNode.removeChild(clientsElement);
+
+ 
+
+    // Insert the #clients element below the target element
+    targetElement.parentNode.insertBefore(clientsElement, targetElement.nextSibling);
+} else {
+    console.log("One or both elements not found.");
+}
