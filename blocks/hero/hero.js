72c46999c58eb9544eb4d80c6edfd9366223a552
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-09-01 11:21:56 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,26 +1,24 @@
-@@ -1,24 +1 @@
-// Get the elements	
-const elementToMove = document.querySelector("body > main > div:nth-child(2) > div");	
-const insertionPoint = document.querySelector("#welcome-to-our-website");	
+// Get the elements
+const elementToMove = document.querySelector("body > main > div:nth-child(2) > div");
+const insertionPoint = document.querySelector("#welcome-to-our-website");
 
+// Check if both elements exist
+if (elementToMove && insertionPoint) {
+  // Insert the element after the insertion point
+  insertionPoint.insertAdjacentElement('afterend', elementToMove);
+} else {
+  console.log("One or both elements not found.");
+}
 
-// Check if both elements exist	
-if (elementToMove && insertionPoint) {	
-  // Insert the element after the insertion point	
-  insertionPoint.insertAdjacentElement('afterend', elementToMove);	
-} else {	
-  console.log("One or both elements not found.");	
-}	
-
-// Select the elements for h3 	
-var clientsElement = document.querySelector("#clients");	
-var targetElement = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div.client.counter.worker");	
-// Check if both elements are valid	
-if (clientsElement && targetElement) {	
-    // Remove the #clients element from its current position	
-    clientsElement.parentNode.removeChild(clientsElement);	
-    // Insert the #clients element below the target element	
-    targetElement.parentNode.insertBefore(clientsElement, targetElement.nextSibling);	
-} else {	
-    console.log("One or both elements not found.");	
+// Select the elements for h3 
+var clientsElement = document.querySelector("#clients");
+var targetElement = document.querySelector("body > main > div:nth-child(5) > div.columns-wrapper > div > div:nth-child(1) > div.client.counter.worker");
+// Check if both elements are valid
+if (clientsElement && targetElement) {
+    // Remove the #clients element from its current position
+    clientsElement.parentNode.removeChild(clientsElement);
+    // Insert the #clients element below the target element
+    targetElement.parentNode.insertBefore(clientsElement, targetElement.nextSibling);
+} else {
+    console.log("One or both elements not found.");
 }
