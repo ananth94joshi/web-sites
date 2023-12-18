31f607741e92a02f2720b61acc5eeb2fd918c591
ananth94joshi <54692069+ananth94joshi@users.noreply.github.com>
2023-12-18 15:11:29 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -275,10 +275,5 @@ function updateContainer() {
 function videoload() {
   $(".hero div div picture").remove();
   $(".hero div div").append("<video width='100%' height='100%' style='position: absolute; top: 0; left: 0;'autoplay controls><source src='movie.mp4' type='video/mp4'></video>");
-}() {
-  videoload();}
-
-function videoload() {
-  $(".hero div div picture").remove();
-  $(".hero div div").append("<video width='100%' height='100%' style='position: absolute; top: 0; left: 0;'autoplay controls><source src='movie.mp4' type='video/mp4'></video>");
 }
+
